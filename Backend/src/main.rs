bff4f8c3deb78ab23c08665e878d44d8478053fb
Rahul721999 <rrmondal12@gmail.com>
2023-04-12 19:41:41 +0530

    working on logging

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -3,12 +3,11 @@ mod config;
 mod start_server;
 use crate::config::Config;
 use start_server::start;
-use tracing::{info, debug};
-
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 #[actix_rt::main]
 async fn main() -> std::io::Result<()>{
-    debug!("main fn called");
     let config = Config::from_env().expect("failed to get the Config");
+    
     start(config).await?;
     Ok(())
 }
--- Backend/src/user_routes/sign_up.rs
+++ Backend/src/user_routes/sign_up.rs
@@ -1,18 +1,15 @@
-#![allow(unused)]
-use std::ptr::hash;
+// #![allow(unused)]
+
 use crate::{
-    models::UserCred,
     utils::hash_pass,
     utils::{AppError, valid_email, valid_password}
 };
-use actix_web::{HttpResponse, web, Responder, App, http::header::HttpDate};
-use anyhow::Context;
+use actix_web::{HttpResponse, web};
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::info;
+use tracing::{info_span, info, error, Instrument};
 use sqlx::types::Uuid;
-use validator::{Validate, ValidationError};
-use regex::Regex;
+use validator::Validate;
 
 #[derive(Debug, Deserialize, Clone, Validate)]
 pub struct NewUser{
@@ -36,34 +33,43 @@ pub async fn sign_up(
     db : web::Data<PgPool>
 )
 -> Result<HttpResponse, AppError>
-{  
+{ 
+// logging
+    let req_id = Uuid::new_v4();
+    // info!("SignUp request: req_id : {}, email: {}, name: {}", req_id, new_user.email.clone(), new_user.first_name.clone());
+    let request_span = info_span!(
+        "Adding a new subscriber.",
+        %req_id,
+        email = %new_user.email.clone(),
+        name= %new_user.first_name.clone()
+        );
+    let _res  = request_span.enter();
 //1. form validation..
     let _res =match new_user.validate(){
         Ok(..) => {},
         Err(err) =>{
-            tracing::info!("validation failed");
-            // return Err(AppError::Other(err))}
+            error!("Validation error");
             match err.field_errors() {
                 errors if errors.contains_key("email")=>{
-                    dbg!(errors);
                     return Err(AppError::AuthError("Invalid email".to_string()))
                 }
                 errors if errors.contains_key("pass") =>{
-                    return Err(AppError::AuthError("Must contain at least one upper-case, one lower-case,\na number & a special char".to_string()))
+                    return Err(AppError::AuthError(format!("Must contain at least one upper-case, one lower-case,
+        a number & a special char")))
                 }
                 _ => return Err(AppError::BadRequest("Invalid input"))
             }   
         }
     };
 //2. first check if the email already present in the DB.
-    let data_present = sqlx::query_scalar::<_, bool>("SELECT EXISTS(SELECT 1 FROM user_cred WHERE email = $1)")
-    .bind(new_user.email.clone())
+    let data_present = sqlx::query_scalar::<_, bool>("SELECT EXISTS(SELECT 1 FROM user_cred WHERE email = $1)").bind(new_user.email.clone())
     .fetch_one(db.as_ref())
+    .instrument(info_span!("Searching for the details in db"))
     .await
-    .map_err(|_| HttpResponse::InternalServerError().body("Failed to check if email exists")).expect("failed to map the error");
+    .map_err(|_| AppError::InternalServerError("Failed to check if email exists".to_string()))?;
 
     if data_present{
-            tracing::info!("Email already present in the db"); 
+            info!("Email : {} already present in the db", new_user.email.clone()); 
             return Err(AppError::EmailExists);
         }
         
@@ -73,7 +79,7 @@ pub async fn sign_up(
         .expect("failed to get the hash");
 
 //4. store email and password hash to the db
-    let res = sqlx::query!(
+    match sqlx::query!(
         "INSERT INTO user_cred (id, email, password_hash, first_name, last_name) VALUES ($1, $2, $3, $4, $5)",
         Uuid::new_v4().into(),
         new_user.email.clone(),
@@ -81,9 +87,10 @@ pub async fn sign_up(
         new_user.first_name.clone(),
         new_user.last_name.clone(),
     ).execute(db.as_ref())
-    .await
-    .expect("failed to execute the query");
-    tracing::info!("new user with email: {} signed in successfuly", new_user.email.clone());
+    .await{
+        Ok(_) =>  info!("{} successfully added",new_user.email.clone()),
+        Err(_) => tracing::error!("Failed to add User")
+    };
 
     Ok(HttpResponse::Ok().body("User added Successfully"))
 }
