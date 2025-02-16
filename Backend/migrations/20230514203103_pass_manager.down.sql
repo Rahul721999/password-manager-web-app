c67b2bb5f320f61d65cc0b31204f13e212d931b6
Rahul721999 <rrmondal12@gmail.com>
2025-02-16 18:41:25 +0530

    refactor: update down migration script to drop indexes before table deletion

--- Backend/migrations/20230514203103_pass_manager.down.sql
+++ Backend/migrations/20230514203103_pass_manager.down.sql
@@ -1,5 +1,6 @@
 -- Add down migration script here
 -- To delete the existing table
-
+DROP INDEX IF EXISTS "idx_users_email";
+DROP INDEX IF EXISTS "idx_id";
 DROP TABLE IF EXISTS "website_credentials";
 DROP TABLE IF EXISTS "user_cred";
